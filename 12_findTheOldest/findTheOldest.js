25f60bcc519680b17602e0732d3f56c3660566d3
Abdulrahman Abdullah <aabdullah014@gmail.com>
2022-04-09 17:03:58 -0700

    Completed exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,10 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+    const deathYear = new Date().getFullYear()
+    const ages = arr.map((obj) => (obj["yearOfDeath"] || deathYear) - obj["yearOfBirth"])
+    const maxAge = Math.max(...ages)
 
+    const ans = arr.find((obj) => (obj["yearOfDeath"] || deathYear) - obj["yearOfBirth"] == maxAge)
+    return ans
 };
 
 // Do not edit below this line
